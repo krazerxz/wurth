9f292f0deb8bae67bfff85b0c1f98aae9a62b939
Chris Pomfret <chris.pomfret@bt.com>
2016-12-30 15:51:10 +0000

    can credit and debit accounts

---
+++ lib/models/account_table.rb
@@ -0,0 +1,16 @@
+require 'terminal-table'
+
+class AccountTable
+  def initialize accounts
+    @accounts = accounts
+  end
+
+  def to_table
+    rows = []
+    rows << Account.attribute_names
+    @accounts.all.each do |account|
+      rows << account.attributes.values
+    end
+    Terminal::Table.new rows: rows
+  end
+end
--- lib/wurth_cli/account_management.rb
+++ lib/wurth_cli/account_management.rb
@@ -1,12 +1,12 @@
 require_relative "../models/account"
-require 'terminal-table'
+require_relative "../models/account_table"
 
 module WurthCLI
   class AccountManagement < Thor
 
     desc "create <name>", "Create an account denoted by <name>"
     def create name
-      Account.create name: name
+      Account.create name: name, balance: 0
       puts "Created account #{name}"
     end
 
@@ -21,20 +21,22 @@ module WurthCLI
       accounts = Account.all
       puts AccountTable.new(accounts).to_table
     end
-  end
-end
 
-class AccountTable
-  def initialize accounts
-    @accounts = accounts
-  end
-
-  def to_table
-    rows = []
-    rows << Account.attribute_names
-    @accounts.all.each do |account|
-      rows << account.attributes.values
+    desc "credit <account> <ammount>", "credits the given account with the given ammount"
+    def credit account_name, value
+      account = Account.find_by name: account_name
+      account.balance -= value.to_f
+      account.save
+      puts "#{account.name} credited #{value}. New balance is #{account.balance}"
     end
-    Terminal::Table.new rows: rows
+
+    desc "debit <account> <ammount>", "debits the given account with the given ammount"
+    def debit account_name, value
+      account = Account.find_by name: account_name
+      account.balance += value.to_f
+      account.save
+      puts "#{account.name} debited #{value}. New balance is #{account.balance}"
+    end
   end
 end
+

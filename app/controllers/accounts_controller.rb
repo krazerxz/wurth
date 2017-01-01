7de57fd87a43cf45b8085e26f4787f3bd3922507
Chris Pomfret <cpom40ba@gmail.com>
2017-01-01 20:16:12 +0000

    update account balance

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -3,6 +3,10 @@ class AccountsController < ApplicationController
     @account_facade = AccountFacade.new Account.all
   end
 
+  def edit
+    @account = Account.find(params[:id])
+  end
+
   def destroy
     account = Account.find(params[:id])
     account.delete
@@ -17,6 +21,15 @@ class AccountsController < ApplicationController
   def create
     @account = Account.new account_params.merge(seed)
     @account.save ? redirect_to(accounts_path) : render(:new)
+  end
+
+  def update
+    @account = Account.find(params[:id])
+    if @account.update(account_params)
+      redirect_to accounts_path
+    else
+      flash_and_render "Count not update account balance", :edit
+    end
   end
 
   private
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def flash_and_redirect message, path
+    flash[:success] = message
+    redirect_to path
+  end
+
+  def flash_and_render message, route
+    flash.now[:danger] = message
+    render route
+  end
 end
